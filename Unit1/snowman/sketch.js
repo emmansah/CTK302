6cc53bd19ca79d4d7ceca434d3d1ef7a8b05bfbb
emmansah <eansah201@gmail.com>
2021-01-19 10:08:16 -0600

    Half Snowman

---
+++ Unit1/snowman/sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+  createCanvas(500, 500);
+}
+
+function draw() {
+  background('gray');
+  ellipse(width/2, height/2 + 100, 150, 150);
+  ellipse(width/2, height/2, 100, 100);
+
+}
