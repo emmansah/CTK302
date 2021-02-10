e125674c61b7ec6ce44546ed41dcf8942cbcd0aa
emmansah <eansah201@gmail.com>
2021-02-10 05:15:43 -0600

    fonts

--- Unit1/fonts/sketch.js
+++ Unit1/fonts/sketch.js
@@ -1,11 +1,15 @@
 
 let f1;
+let f2;
+let f3;
 //defines the variable/letter?? as f1
 
 function setup() {
   createCanvas(500, 500);
   textAlign(CENTER);
   f1 = loadFont("assets/secretWinter.ttf");
+  f2 = loadFont("assets/aurelieSmith.ttf");
+  f3 = loadFont("assets/browniesCake.ttf");
   //load command
     //used for images, sounds, videos
     //all load commands go in the setup
@@ -16,4 +20,12 @@ function draw() {
   textSize(48);
   textFont(f1);
   text("hello", width/2, height/2);
+
+  textFont(f2);
+  text("hello", width/2, height/2+40);
+
+  textFont(f3);
+  text("hello", width/2, height/2+80);
+
+
 }
