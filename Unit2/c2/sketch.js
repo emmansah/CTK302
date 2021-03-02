f9832248e29d8c7a1732b88abd4e5f9de782cb38
emmansah <eansah201@gmail.com>
2021-03-01 22:05:35 -0600

    c2 kicking my ass why

--- Unit2/c2/sketch.js
+++ Unit2/c2/sketch.js
@@ -1,19 +1,59 @@
-let state = 0;
+let state = -1;
+let mic;
+let vol;
+let timer = 0;
 
 function setup(){
   createCanvas(500, 500);
+  textAlign(CENTER);
+
+  //code for initializing mic in
+  mic = new p5.AudioIn();
+  mic.start();
 }
 
 function draw() {
 
-  switch(state_{
+  //get the sound input
+  vol = (mic.getLevel()).toFixed(2);
+  //(mic.getLevel()) gets mic stuff
+  //.toFixed(2) rounds to 2 decimal places :)
+
+  switch(state) {
+
+    case -1:
+    background(100);
+    text("click to start", width/2, height/2);
+    break;
+
+
     case 0:
+    background(100);
+    text("SHHHHH!!!!!", width/2, height/2);
+    if(vol > .3) {
+      state = 1;
+    }
     break;
 
     case 1:
+    background('red');
+    text("QUIET DOWN!!!!", width/2, height/2);
+    timer++;
+    if (timer > 3*60) {
+      state = 0;
+      timer = 0;
+    }
     break;
 
-
-
   }
+}
+
+function mouseReleased(){
+  if(state == -1){
+    state = 0;
+  }
+}
+
+function touchStarted(){
+  getAudioContext().resume();
 }
