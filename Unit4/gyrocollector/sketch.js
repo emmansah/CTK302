36d266359398921316cc327f2a2641ddd2aafdf4
emmansah <eansah201@gmail.com>
2021-04-26 01:01:44 -0500

    gc working?

--- Unit4/gyrocollector/sketch.js
+++ Unit4/gyrocollector/sketch.js
@@ -1,78 +1,86 @@
-var beta, gamma;
-var x = 0;
+// variables needed for gyroscope
+var beta, gamma; // orientation data
+var x = 0; // acceleration data
 var y = 0;
 var z = 0;
 var xPosition = 0;
 var yPosition = 0;
 
+// var bunnyImage;
 var cars = [];
 var frogPos;
 
 
-
-function preload() {
-  oil = loadImage("assets/oil_.png");
-  gold = loadImage("assets/gold_.png");
-  diamond = loadImage("assets/diamond_.png");
-  spices = loadImage("assets/spices_.png");
-  myPix = [oil, gold, diamond, spices];
-
-}
-
-
 function setup() {
+
   createCanvas(windowWidth, windowHeight);
 
+  // initialize accelerometer variables
   alpha = 0;
   beta = 0;
   gamma = 0;
 
-  for(var i = 0; i < 40; i++){
+
+  // spawn a bunch of cars
+  for (var i = 0; i < 40; i++) {
     cars.push(new Car());
   }
 
+  // initialize the frog's position
+  frogPos = createVector(width / 2, height - 80);
 
-  fromPos = createVector(width/2, height - 80);
-
-  imadeMode(CENTER);
+  // load any images you need
+  //bunnyImage = loadImage("assets/bunny.jpg");
+  imageMode(CENTER);
   rectMode(CENTER);
   noStroke();
 }
 
 function draw() {
 
-  background('#c6f5ff');
+  background('#c6f5ff'); // light blue
 
+  // the map command !!!!
+  // takes your variable and maps it from range 1 to range 2
+  // map(yourVar, range1_x, range1_y, range2_x, range2_y) ;
   xPosition = map(gamma, -18, 18, 0, width);
   yPosition = map(beta, 25, 45, 0, height);
 
-  push();
-  translate(xPosition, yPosition);
 
+  // move the frog around the screen
+  push(); // before you use translate, rotate, or scale commands, push and then pop after
+  translate(xPosition, yPosition); // move everything over by x, y
+  //  rotate(radians(alpha)); // using alpha in here so it doesn't feel bad
 
+  // draw the FROG
+  // image(bunnyImage, 0, 0, 500, 500);
   fill('green');
   ellipse(0, 0, 80, 80);
   pop();
 
+
+  // update the frog's position using the accelerometer data
   frogPos.x = xPosition;
   frogPos.y = yPosition;
 
-
-  for(var i = 0; i < cars.length; i++){
+  // iterate through the car loop to move them and see if we need to delete cars
+  for (var i = 0; i < cars.length; i++) {
     cars[i].display();
     cars[i].drive();
-    if(cars[i].pos.dist(frogPos) < 50){
+    if (cars[i].pos.dist(frogPos) < 50) {
       cars.splice(i, 1);
     }
   }
 
-
+  // MORE DECORATIONS - write that pretty ATK type on top.
   fill('white');
   textSize(40);
   textAlign(CENTER);
-  text("your words/image here", width/2, 600, windowWidth - 200, windowHeight - 200);
+  text("your words or image here!", width / 2, 600, windowWidth - 200, windowHeight - 200);
 
 
+  // Debugging information -- take this out when you're ready for production!
+  // Just a bunch of text commands to display data coming in from addEventListeners
   textAlign(LEFT);
   textSize(20);
   fill('black');
@@ -86,25 +94,31 @@ function draw() {
   textSize(15);
   text("x = " + x, 25, 150); // .toFixed means just show (x) decimal places
   text("y = " + y, 25, 170);
-  text("z = " + z), 25, 190);
+  text("z = " + z, 25, 190);
+
+
 }
 
-
-function deviceShaken(){
-  cars = [];
-  for (var i = 0; i < 40; i++){
+function deviceShaken() {
+  // re-spawn cars
+  cars = []; // clear the array first
+  for (var i = 0; i < 40; i++) {
     cars.push(new Car());
   }
 }
 
 
+// HERE'S THE STUFF YOU NEED FOR READING IN DATA!!!
+
+// Read in accelerometer data
 window.addEventListener('deviceorientation', function(e) {
   alpha = e.alpha;
   beta = e.beta;
   gamma = e.gamma;
 });
 
-// Read in accelerometer data
+
+// accelerometer Data
 window.addEventListener('devicemotion', function(e) {
   // get accelerometer values
   x = e.acceleration.x;
@@ -115,37 +129,37 @@ window.addEventListener('devicemotion', function(e) {
 
 
 
+
+// car class!!
 function Car() {
-
-    // constructor and attributes
-      this.pos = createVector(100, 100);
-      this.vel = createVector(random(-4, 4), random(-4, 4));
-      this.r = random(255);
-      this.g = random(255);
-      this.b = random(255);
-      this.a = random(255);
-        //random image from myPix array
+  // attributes
+  this.pos = createVector(100, 100);
+  this.vel = createVector(random(-5, 5), random(-5, 5));
+  this.r = random(255);
+  this.g = random(255);
+  this.b = random(255);
+  this.a = random(255);  // alpha opacity value for fill!
 
 
-    // methods
+  // methods
+  this.display = function() {
 
-    this.display = function() {
-        fill(this.r, this.g, this.b, this.a);
-        ellipse(this.pos.x, this.pos.y, 50, 50); //??
-        ellipse(this.pos.x + 35, this.pos.y, 50, 50);
-        rect(this.pos.x + 17, this.pos.y - 30, 80, 60);
-    }
+    // maybe use an image here instead!
+    fill(this.r, this.g, this.b, this.a);
+    ellipse(this.pos.x - 50, this.pos.y, 50, 50);
+    ellipse(this.pos.x + 50, this.pos.y, 50, 50);
+    rect(this.pos.x + 17, this.pos.y - 30, 80, 60) ;
 
+  }
 
+  this.drive = function() {
+    this.pos.add(this.vel);
 
-    this.drive = function() {
-      this.pos.add(this.vel);
+    if (this.pos.x > width) this.pos.x = 0;
+    if (this.pos.x < 0) this.pos.x = width;
+    if (this.pos.y > height) this.pos.y = 0;
+    if (this.pos.y < 0) this.pos.y = height;
 
-      if (this.pos.x > width) this.pos.x = 0;
-      if (this.pos.x < 0) this.pos.x = width;
-      if (this.pos.y > height) this.pos.y = 0;
-      if (this.pos.y < 0) this.pos.y = height;
-
-    }
+  }
 
 }
