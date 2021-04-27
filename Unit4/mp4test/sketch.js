7527e7e5b59e59a9b1f12970735833b8b59eb85d
emmansah <eansah201@gmail.com>
2021-04-27 04:18:24 -0500

    button

--- Unit4/mp4test/sketch.js
+++ Unit4/mp4test/sketch.js
@@ -50,6 +50,8 @@ function setup() {
   myInput.position(width/2-width/6, height/2 + height/3.5);
 
   button = createButton('submit');
+  button.size(80);
+  button.style('background-color', 'white');
   button.position(width/2-width/18, height/1.5 + height/4.5);
 
   setShakeThreshold(threshold);
