5e99e90e17d3248170c298dbe49764f9cf18c609
emmansah <eansah201@gmail.com>
2021-04-28 04:01:18 -0500

    weather

--- Unit4/weather/sketch.js
+++ Unit4/weather/sketch.js
@@ -7,6 +7,8 @@ var x = 0;
 var windspeed = 0 ;
 var temp = 0;
 var desk = "";
+var feelsLike = 0;
+var hum = 0;
 
 function setup() {
   createCanvas(400, 400);
@@ -31,9 +33,10 @@ function gotData(data) {
   weather = data;
   console.log(weather); // for debugging purposes, print out the JSON data when we get it.
   windspeed = weather.wind.speed;
-  temp = weather.main.temp;
+  temp = round(weather.main.temp);
+  feelsLike = round(weather.main.feels_like);
   desc = weather.weather[0].description;
-
+  hum = weather.main.humidity;
 }
 
 
@@ -52,13 +55,6 @@ function draw() {
       background(163, 194, 207);
       fill(169, 207, 163);
       rect(0, 250, width, height);
-      fill('black');
-      text("What is the weather in " + weather.name + "?", width/2, 280);
-      text("windspeed is " + windspeed, width/2, 325);
-      text("temperature is " + temp, width/2, 350);
-      text("description: " + desc, width/2, 375);
-
-
 
       // cloud
       fill('white');
@@ -68,19 +64,55 @@ function draw() {
       ellipse(x-60, 20, 150, 75);
 
 
-      //thermometer
-      fill('red');
-
-      var t = map(temp, -10, 100, 10, height-10);
-      rect(width-50, height-10, 30, -t);
-
-      stroke('black');
-      fill('white');
-      rect(width-30, 10, 20, 380)
-
       // move the cloud's x position
       x = x + windspeed/4;
       if (x-300 > width) x = 0;
+
+
+
+      fill('black');
+      textSize(30);
+      text(weather.name, width/2, 50);
+      textSize(90);
+      text(temp + "°F", width/2, 180);
+      textSize(24);
+      text("feels like " + feelsLike + "°F", width/2-2, 220);
+      textSize(20);
+      text(desc, width/2, 300);
+      text("windspeed is " + windspeed + "mph", width/2, 330);
+      text("humidity is " + hum+ "%", width/2, 360);
+
+
+
+
+      //thermometer
+
+
+      stroke('black');
+      fill('white');
+      rect(width-24, 10, 16, 380);
+
+      fill('red');
+
+      var t = map(temp, -10, 100, 10, height-10);
+      rect(width-24, height-8, 16, -t);
+
+      fill('black');
+      textSize(14);
+    //  text(temp +"F", width-52, -t);
+      text("-10°F -", width-52, height-7);
+      text("0°F -", width-47, height-43);
+      text("10°F -", width-50, height-78);
+      text("20°F -", width-50, height-113);
+      text("30°F -", width-50, height-148);
+      text("40°F -", width-50, height-183);
+      text("50°F -", width-50, height-218);
+      text("60°F -", width-50, height-253);
+      text("70°F -", width-50, height-288);
+      text("80°F -", width-50, height-323);
+      text("90°F -", width-50, height-358);
+      text("100°F -", width-53, height-387);
+
 
 
       break;
